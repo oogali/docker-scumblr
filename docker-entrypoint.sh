ba7ebf93755448338f1bea53eeeaf5b52f206afd
Omachonu Ogali <oogali@gmail.com>
2018-03-04 12:37:15 -0500

    It's been two years... throw out everything.

--- docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source /etc/profile.d/rbenv.sh
-exec 2>&1
-
-exec /bin/bash --login -c "$@"
--- etc-profile.d-rbenv.sh
+++
@@ -1,8 +0,0 @@
-# Set rbenv root
-export RBENV_ROOT="/usr/local/rbenv"
-
-# Append rbenv to end of PATH
-export PATH="${PATH}:${RBENV_ROOT}/bin"
-
-# Load rbenv
-eval "$(rbenv init -)"
