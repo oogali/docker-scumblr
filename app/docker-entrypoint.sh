a78253db270efccdc0e686f163124eb784268207
Omachonu Ogali <oogali@gmail.com>
2018-03-04 14:41:43 -0500

    Use password to connect to database, and proper syntax to check if "tasks" table exists

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 exec 2>&1
 
 if [ -z "${SKIP_MIGRATIONS}" ]; then
-  psql -c "SELECT 1 FROM tasks LIMIT 1;" -U "${DB_USERNAME}" --no-password -h "${DB_HOST}" -p "${DB_PORT}" "${DB_NAME}" >/dev/null || (
+  PGPASSWORD="${DB_PASSWORD}" psql -c "SELECT 'tasks'::regclass" -U "${DB_USERNAME}" -h "${DB_HOST}" -p "${DB_PORT}" "${DB_NAME}" >/dev/null || (
     rake db:create && \
       rake db:structure:load
   )
